46fc4fa8ecd005548afa478dcd232ccb76bc5bdf
Ella Gading <ellagading@macbook-pro.myfiosgateway.com>
2022-01-13 18:06:21 -0500

    first commit

--- mecurySedan.js
+++ mecurySedan.js
@@ -1,4 +1,45 @@
 //this includes the vehicle class as a module
+import { Vehicle } from "./vehicleBaseClass";
+
+class Car extends Vehicle {
+    constructor(make, model, year, color, mileage){
+        super(make, model, year, color, mileage);
+        this.maxPassengers = 6;
+        this.passenger = 0;
+        this.numberOfWheels = 4;
+        this.maxSpeed = 200;
+        this.fuel = 100;
+        this.scheduleService = false;
+    }
+
+    start() {
+        if (this.fuel > 0) {
+            return this.started = true;
+            console.log("engine started...!!!");
+        } else {
+            return this.started = false;
+            console.log("engine cannot start...");
+        }
+}
+    loadPassenger(num) {
+        if (this.passenger < this.maxPassengers) {
+            if ((num + this.passenger) <= this.maxPassengers) {
+                this.passenger = num;
+                return this.passenger;
+            } else {
+                console.log(this.model + " " + this.make + "not have enough space to take all passengers.");
+            }
+        } else {
+            console.log(this.model + " " + this.make + "is full");
+        }
+    }
+    scheduleService(mileage) {
+        if (this.mileage > 30000) {
+            this.scheduleService == true
+            return this.scheduleService;
+        }
+    }
+
 const VehicleModule = require("./vehicle")
 
 //this shows how to call from this module...
